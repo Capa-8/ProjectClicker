fb8a301bc58477f1cd7e262dcf74e5d7cd381155
Fabio Gazzoni <fabiogazzoni96@gmail.com>
2021-06-12 17:32:40 -0300

    Solucion de test

--- clickcoin/src/test/java/clicker/EstadisticasTest.java
+++ clickcoin/src/test/java/clicker/EstadisticasTest.java
@@ -101,13 +101,13 @@ public class EstadisticasTest {
         System.out.println("quitar BTC");
         Juego juego = new Juego(new Jugador("test"));
         juego.getEstadisticas().initSubject((MonedaBTC) juego.getMonedaBTC());
-        int i = 300;
+        int i = 200;
         for (int j = 0; j < i; j++) {
             juego.getMinado().realizarMinado();//aca un observer
         }
 
         ArrayList<Enemigo> enemigos = juego.getFabricaE().getEnemigos();
-        Enemigo ene = enemigos.get(0);
+        Enemigo ene = enemigos.get(2);
         ene.atacar();
 
         float cantMonedasBTC = juego.getEstadisticas().getMonedasBTC();
@@ -126,14 +126,14 @@ public class EstadisticasTest {
         Moneda moneda = (MonedaETH) juego.getMonedaETH();
         juego.getMinado().setMoneda(moneda);
         juego.getEstadisticas().initSubject((MonedaETH) moneda);
-        int i = 300;
+        int i = 200;
         for (int j = 0; j < i; j++) {
             juego.getMinado().realizarMinado();
         }
 
       
         ArrayList<Enemigo> enemigos = juego.getFabricaE().getEnemigos();
-        Enemigo ene = enemigos.get(0);
+        Enemigo ene = enemigos.get(2);
         ene.atacar();
 
         float cantMonedasETH = juego.getEstadisticas().getMonedasETH();
